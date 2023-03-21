ca92f78d00bb159c338bceca5465c820ac133140
hmdoganjf <88782101+hmdoganjf@users.noreply.github.com>
2023-03-21 18:09:03 +0300

    fix: endpoint is not used in prod

--- src/classes/RemoteCacher.js
+++ src/classes/RemoteCacher.js
@@ -12,7 +12,6 @@ class RemoteCacher {
   constructor() {
     this.s3Client = new S3({
       region: "us-east-1",
-      endpoint: process.env.S3_ENDPOINT,
       credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY,
         secretAccessKey: process.env.S3_SECRET_KEY,
