f58ff5954b846fe507ed4ba48d8b58475fc15a88
hmdoganjf <88782101+hmdoganjf@users.noreply.github.com>
2023-04-10 11:21:29 +0300

    remove jotform package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: '@jotforminc/eslint-config-base-ts'
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended',
+  'plugin:@typescript-eslint/recommended-requiring-type-checking',],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
 };
