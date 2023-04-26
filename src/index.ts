512f3ea710c33c20c8c49d3d4ca86e006a09e210
hmdoganjf <88782101+hmdoganjf@users.noreply.github.com>
2023-04-26 11:20:44 +0300

    test: missing package json (#35)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import Runner from './classes/Runner.js';
-
-const run = async () => {
-  try {
-    const args = process.argv;
-    const RunnerHelper = new Runner(...args);
-    await RunnerHelper.run();
-  } catch (error) {
-    console.log('ERR => R-I ::');
-    if (error instanceof Error) throw error;
-    throw String(error);
-  }
-};
+import { run } from "./run";
 
 void run();
---
+++ tests/BuildHelper.spec.js
@@ -0,0 +1,23 @@
+import { run } from '../build/run';
+import ConfigHelperInstance from '../build/classes/ConfigHelper';
+
+const mockArgs = [
+    '--target=build',
+    '--project=all'
+]
+
+const mockProjects = {
+    "@jotforminc/zenith": ".",
+    "@jotforminc/mockProject": "src"
+}
+
+describe('BuildHelper tests', () => {
+    test('Should throw error when a project is missing package.json', async () => {
+        // simulate as if we are running `zenith --target=build --project=all`
+        const oldArgs = [...process.argv];
+        process.argv = process.argv.slice(0,2).concat(mockArgs);
+        ConfigHelperInstance.projects = mockProjects;
+        await expect(run()).rejects.toThrow();
+        process.argv = [...oldArgs];
+    })
+})
