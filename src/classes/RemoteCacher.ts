1310becfbb9c1f7fc8754f5558af8ec85f29868d
hmdoganjf <mertdogan@jotform.com>
2023-09-18 14:11:38 +0300

    fix: remove redundant console log

--- src/classes/RemoteCacher.ts
+++ src/classes/RemoteCacher.ts
@@ -139,7 +139,6 @@ class RemoteCacher {
           reject(err);
         }
         Logger.log(3, 'Cache successfully stored');
-        Logger.log(2, commandOutput);
         resolve();
       }
     ));
